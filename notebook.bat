93bd95f32c16d565d0c49863eb60316d89b29820
lsorber <laurent.sorber@gmail.com>
2016-07-05 15:49:29 +0200

    Move to EU, fixed bugs

--- notebook.bat
+++ notebook.bat
@@ -1,21 +1,20 @@
 REM Create the EC2 spot instance.
 docker-machine create supercomputer ^
     --driver amazonec2 ^
-    --amazonec2-region us-east-1 ^
-    --amazonec2-zone d ^
+    --amazonec2-region eu-west-1 ^
+    --amazonec2-zone b ^
     --amazonec2-request-spot-instance ^
     --amazonec2-spot-price 1.0 ^
     --amazonec2-security-group docker-notebook ^
     --amazonec2-instance-type c4.8xlarge
 
 REM Activate the spot instance as our current docker machine.
-@FOR /f "tokens=*" %i IN ('docker-machine env supercomputer') DO @%i
+@FOR /f "tokens=*" %%i IN ('docker-machine env supercomputer') DO @%%i
 
 REM Attach the persistent EBS volume to the instance.
 @FOR /f "delims=" %%i in ('docker-machine ssh supercomputer wget -q -O - http://instance-data/latest/meta-data/instance-id') do SET EC2_INSTANCE_ID=%%i
-@FOR /f "delims=" %%i in ('aws ec2 describe-volumes --query "Volumes[*].[VolumeId]" --filters Name=tag:Name,Values=docker-notebook --region us-east-1 --output text
-') do SET EBS_VOLUME_ID=%%i
-aws ec2 attach-volume --volume-id %EBS_VOLUME_ID% --instance-id %EC2_INSTANCE_ID% --device /dev/xvdf --region us-east-1
+@FOR /f "delims=" %%i in ('aws ec2 describe-volumes --query "Volumes[*].[VolumeId]" --filters "Name=tag:Name,Values=docker-notebook" --region eu-west-1 --output text') do SET EBS_VOLUME_ID=%%i
+aws ec2 attach-volume --volume-id %EBS_VOLUME_ID% --instance-id %EC2_INSTANCE_ID% --device /dev/xvdf --region eu-west-1
 docker-machine ssh supercomputer "sudo mkdir /data && sudo mount /dev/xvdf /data"
 
 REM Run the Jupyter notebook.
--- notebook.sh
+++ notebook.sh
@@ -1,8 +1,8 @@
 # Create the EC2 spot instance.
 docker-machine create supercomputer \
     --driver amazonec2 \
-    --amazonec2-region us-east-1 \
-    --amazonec2-zone d \
+    --amazonec2-region eu-west-1 \
+    --amazonec2-zone b \
     --amazonec2-request-spot-instance \
     --amazonec2-spot-price 1.0 \
     --amazonec2-security-group docker-notebook \
@@ -13,8 +13,8 @@ eval "$(docker-machine env supercomputer)"
 
 # Attach the persistent EBS volume to the instance.
 EC2_INSTANCE_ID="`docker-machine ssh supercomputer wget -q -O - http://instance-data/latest/meta-data/instance-id`"
-EBS_VOLUME_ID="`aws ec2 describe-volumes --query "Volumes[*].[VolumeId]" --filters Name=tag:Name,Values=docker-notebook --region us-east-1 --output text`"
-aws ec2 attach-volume --volume-id $EBS_VOLUME_ID --instance-id $EC2_INSTANCE_ID --device /dev/xvdf --region us-east-1
+EBS_VOLUME_ID="`aws ec2 describe-volumes --query "Volumes[*].[VolumeId]" --filters "Name=tag:Name,Values=docker-notebook" --region eu-west-1 --output text`"
+aws ec2 attach-volume --volume-id $EBS_VOLUME_ID --instance-id $EC2_INSTANCE_ID --device /dev/xvdf --region eu-west-1
 docker-machine ssh supercomputer "sudo mkdir /data && sudo mount /dev/xvdf /data"
 
 # Run the Jupyter notebook.
