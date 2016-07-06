2674b409d12b6110ebf9a2f9f6141bc61cd0e449
lsorber <laurent.sorber@gmail.com>
2016-07-06 10:20:48 +0200

    Stop if EBS volume already attached. Automatic account id retrieval. Use CloudFlare ssl.

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ## One-time setup
 
-1. Install the AWS CLI with `pip install awscli`. Install your AWS credentials under `~/.aws`.
-2. Create an EBS volume in the availability zone `eu-west-1b` in the AWS console and give it the name `docker-notebook`.
+1. Install the AWS CLI with `pip install awscli`. Install your AWS credentials under `~/.aws/credentials`.
+2. Create an EBS volume in the availability zone `eu-west-1a` in the AWS console and give it the name `docker-notebook`.
 3. After creation, an EBS volume is a raw block volume. To create a file system on it, run `docker-machine ssh supercomputer sudo mkfs -t ext4 /dev/xvdf` right after attaching the volume.
-4. Add an inbound rule to the `docker-notebook` security group in the AWS console to allow traffic from port 443 and 80.
+4. Add an inbound rule to the `docker-notebook` security group in the AWS console to allow traffic from port 80 and 443.
 5. Set the environment variables `CLOUDFLARE_API_KEY` and `FORESPELL_NOTEBOOK_PASSWORD`. On Linux, use `~/.bashrc`, on macOS use `~/.bash_profile` and on Windows use `setx`.
 
 ## Starting and stopping the notebook server
--- notebook.bat
+++ notebook.bat
@@ -1,12 +1,19 @@
+REM Quit if the EBS volume is already attached or not available.
+@FOR /f "delims=" %%i in ('aws ec2 describe-volumes --query "Volumes[*].[State]" --filters "Name=tag:Name,Values=docker-notebook" --region eu-west-1 --output text') do SET EBS_VOLUME_STATE=%%i
+@IF NOT "%EBS_VOLUME_STATE%" == "available" (
+    ECHO Notebook server already running!
+    EXIT /B
+)
+
 REM Create the EC2 spot instance.
 docker-machine create supercomputer ^
     --driver amazonec2 ^
     --amazonec2-region eu-west-1 ^
-    --amazonec2-zone b ^
+    --amazonec2-zone a ^
     --amazonec2-request-spot-instance ^
     --amazonec2-spot-price 1.0 ^
     --amazonec2-security-group docker-notebook ^
-    --amazonec2-instance-type c4.8xlarge
+    --amazonec2-instance-type c4.4xlarge
 
 REM Activate the spot instance as our current docker machine.
 @FOR /f "tokens=*" %%i IN ('docker-machine env supercomputer') DO @%%i
@@ -22,7 +29,8 @@ REM Point notebook.forespell.com to the notebook server.
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/27be6cf860eca466a0b1cdcadd719544/dns_records/1bbcf7bc8625624b9977c851cf38c409" -H "X-Auth-Email: devs@forespell.com" -H "X-Auth-Key: %CLOUDFLARE_API_KEY%" -H "Content-Type: application/json" --data "{\"id\":\"1bbcf7bc8625624b9977c851cf38c409\",\"type\":\"A\",\"name\":\"notebook.forespell.com\",\"content\":\"%EC2_INSTANCE_IP%\"}"
 
 REM Run the Jupyter notebook.
-docker run -d -p 443:8888 -v /data:/home/jovyan/work -e USE_HTTPS=yes -e PASSWORD=%FORESPELL_NOTEBOOK_PASSWORD% forespell/docker-notebook
+docker run -d -p 80:8888 -v /data:/home/jovyan/work -e USE_HTTPS=no -e PASSWORD=%FORESPELL_NOTEBOOK_PASSWORD% forespell/docker-notebook
 
 REM Set a CloudWatch alarm that terminates the notebook server after 2 hours of inactivity.
-aws cloudwatch put-metric-alarm --alarm-name cpu-mon --alarm-description "Alarm when CPU is idle for 2 hours" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Maximum --period 1800 --threshold 1 --comparison-operator LessThanThreshold  --dimensions "Name=InstanceId,Value=%EC2_INSTANCE_ID%" --evaluation-periods 4 --alarm-actions arn:aws:swf:eu-west-1:422646463147:action/actions/AWS_EC2.InstanceId.Terminate/1.0 --unit Percent
+@FOR /f "delims=" %%i in ('aws ec2 describe-security-groups --group-names default --query "SecurityGroups[0].OwnerId" --output text') do SET AWS_ACCOUNT_ID=%%i
+aws cloudwatch put-metric-alarm --alarm-name cpu-mon --alarm-description "Alarm when CPU is idle for 2 hours" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Maximum --period 1800 --threshold 1 --comparison-operator LessThanThreshold  --dimensions "Name=InstanceId,Value=%EC2_INSTANCE_ID%" --evaluation-periods 4 --alarm-actions arn:aws:swf:eu-west-1:%AWS_ACCOUNT_ID%:action/actions/AWS_EC2.InstanceId.Terminate/1.0 --unit Percent
